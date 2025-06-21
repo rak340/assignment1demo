51ec356c5b146351a22ad48d38f739d5750ba65a
rak340 <rak276@gmail.com>
2025-06-20 21:35:54 -0400

    updated code for app.py

--- app.py
+++ app.py
@@ -1,5 +1,2 @@
-from datetime import datetime
+print("App is running inside Docker container!")
 
-print("✅ Hello Rakesh!")
-print("🚀 This app is running inside a Docker container, built automatically by GitHub Actions.")
-print("🕒 Current time:", datetime.now())
